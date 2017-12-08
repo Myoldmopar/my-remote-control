03040e44218607e4d388eae03cb2f53165dafd8c
Edwin Lee <leeed2001@gmail.com>
2017-12-08 14:19:26 -0600

    Multiple controllers

--- controllerapp/static/js/controllerapp.js
+++ controllerapp/static/js/controllerapp.js
@@ -1,6 +1,6 @@
-angular.module('volumeApp', [])
-    .controller('volumeController', ['$scope', '$http', function ($scope, $http) {
-        $scope.count = 0;
+var app = angular.module('controllerApp', []);
+
+app.controller('volumeController', ['$scope', '$http', function ($scope, $http) {
         $scope.volumeUp = function () {
             $http.get('/api/volume/up/');
         };
@@ -10,6 +10,9 @@ angular.module('volumeApp', [])
         $scope.volumeMute = function () {
             $http.get('/api/volume/toggle_mute/');
         };
+    }]);
+
+app.controller('mediaController', ['$scope', '$http', function ($scope, $http) {
         $scope.playPause = function () {
             $http.get('/api/media/play_pause/');
         };
