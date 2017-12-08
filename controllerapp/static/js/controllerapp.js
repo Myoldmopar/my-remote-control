97e5ace5ff9ca0c15936387e8e9eaab33d247b6c
Edwin Lee <leeed2001@gmail.com>
2017-12-08 14:22:29 -0600

    Formatting

--- controllerapp/static/js/controllerapp.js
+++ controllerapp/static/js/controllerapp.js
@@ -1,28 +1,28 @@
 var app = angular.module('controllerApp', []);
 
 app.controller('volumeController', ['$scope', '$http', function ($scope, $http) {
-        $scope.volumeUp = function () {
-            $http.get('/api/volume/up/');
-        };
-        $scope.volumeDown = function () {
-            $http.get('/api/volume/down/');
-        };
-        $scope.volumeMute = function () {
-            $http.get('/api/volume/toggle_mute/');
-        };
-    }]);
+    $scope.volumeUp = function () {
+        $http.get('/api/volume/up/');
+    };
+    $scope.volumeDown = function () {
+        $http.get('/api/volume/down/');
+    };
+    $scope.volumeMute = function () {
+        $http.get('/api/volume/toggle_mute/');
+    };
+}]);
 
 app.controller('mediaController', ['$scope', '$http', function ($scope, $http) {
-        $scope.playPause = function () {
-            $http.get('/api/media/play_pause/');
-        };
-        $scope.nextSong = function () {
-            $http.get('/api/media/next_song/');
-        };
-        $scope.openPithos = function () {
-            $http.get('/api/media/open_pithos/');
-        };
-        $scope.closePithos = function () {
-            $http.get('/api/media/close_pithos/');
-        };
-    }]);
+    $scope.playPause = function () {
+        $http.get('/api/media/play_pause/');
+    };
+    $scope.nextSong = function () {
+        $http.get('/api/media/next_song/');
+    };
+    $scope.openPithos = function () {
+        $http.get('/api/media/open_pithos/');
+    };
+    $scope.closePithos = function () {
+        $http.get('/api/media/close_pithos/');
+    };
+}]);
