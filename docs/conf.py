ba06a765041b8660e7de4930af3cf7f28e1b6b7b
Edwin Lee <leeed2001@gmail.com>
2017-12-11 15:30:28 -0600

    Install proper deps for spelling

--- docs/conf.py
+++ docs/conf.py
@@ -44,7 +44,7 @@ extensions = ['sphinx.ext.autodoc',
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
-spelling_word_list_filename='spelling_wordlist.txt'
+spelling_word_list_filename = 'spelling_wordlist.txt'
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
