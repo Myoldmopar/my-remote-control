8abe5ddda5faf83951aa749d7fd6b03920f6c05f
Edwin Lee <leeed2001@gmail.com>
2017-12-11 15:02:12 -0600

    Add spelling test; will fail

--- docs/conf.py
+++ docs/conf.py
@@ -38,10 +38,13 @@ for mod_name in MOCK_MODULES:
 # ones.
 extensions = ['sphinx.ext.autodoc',
               'sphinx.ext.todo',
-              'sphinx.ext.coverage']
+              'sphinx.ext.coverage',
+              'sphinxcontrib.spelling']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
+
+spelling_word_list_filename='spelling_wordlist.txt'
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
