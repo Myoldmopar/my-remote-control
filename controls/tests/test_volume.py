dd1c7435f17d0a5458b98f1863cc73bf8b3b537d
Edwin Lee <leeed2001@gmail.com>
2017-12-11 18:58:00 -0600

    Should be 100% coverage, so to speak

--- controls/tests/test_volume.py
+++ controls/tests/test_volume.py
@@ -25,3 +25,47 @@ class TestVolumeController(TestCase):
             self.assertIsInstance(volume_value, int)
         except IndexError:
             self.fail('Something wrong with volume response in get_volume, some underlying Mixer problem.')
+
+    # TODO: add more local run tests
+
+
+class TestSuccessfulVolumeReturnType(TestCase):
+    def setUp(self):
+        self.volume = 8
+        self.s = volume.SuccessfulVolumeReturnType(self.volume)
+
+    def test_structure(self):
+        if not hasattr(self.s, 'success'):
+            self.fail('SuccessfulVolumeReturnType does not contain success member.')
+        self.assertTrue(self.s.success)
+        if not hasattr(self.s, 'volume'):
+            self.fail('SuccessfulVolumeReturnType does not contain volume member.')
+        self.assertEqual(self.volume, self.s.volume)
+
+    def test_to_dict(self):
+        d = self.s.to_dict()
+        self.assertIn('success', d)
+        self.assertTrue(d['success'])
+        self.assertIn('volume', d)
+        self.assertEqual(self.volume, d['volume'])
+
+
+class TestSuccessfulMuteReturnType(TestCase):
+    def setUp(self):
+        self.mute = volume.VolumeController.Muted
+        self.s = volume.SuccessfulMuteReturnType(self.mute)
+
+    def test_structure(self):
+        if not hasattr(self.s, 'success'):
+            self.fail('SuccessfulMuteReturnType does not contain success member.')
+        self.assertTrue(self.s.success)
+        if not hasattr(self.s, 'muted'):
+            self.fail('SuccessfulMuteReturnType does not contain muted member.')
+        self.assertEqual(self.mute, self.s.muted)
+
+    def test_to_dict(self):
+        d = self.s.to_dict()
+        self.assertIn('success', d)
+        self.assertTrue(d['success'])
+        self.assertIn('muted', d)
+        self.assertEqual(self.mute, d['muted'])
